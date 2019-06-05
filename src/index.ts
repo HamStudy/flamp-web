d2980b1b802fbe14836b71e6c53d38d6f0d37c45
Richard Bateman <taxilian@gmail.com>
2019-06-04 19:55:50 -0600

    Update to use latest mt63-wasm to get resampler; updates to make work with racecontrol

--- src/index.ts
+++ src/index.ts
@@ -16,12 +16,23 @@ import {
 } from '@hamstudy/flamp/dist/compressor';
 
 export const wasmPath = require('@hamstudy/mt63-wasm/dist/mt63Wasm.wasm');
-setFileLocation('mt63Wasm.wasm', wasmPath);
-initMt63().then(() => {
-    let mtc = new MT63Client();
-    Compressor.addCompressor({
-        prefix: "\u0001LZMA",
-        compress: mtc.lzmaEncode,
-        decompress: mtc.lzmaDecode
-    });
-});
+
+let rootPath = "";
+let initPromise: Promise<MT63Client>;
+export function init(path?: string) {
+    if (!initPromise) {
+        setFileLocation('mt63Wasm.wasm', path + wasmPath);
+        initPromise = initMt63().then(() => {
+            let mtc = new MT63Client();
+            Compressor.addCompressor({
+                prefix: "\u0001LZMA",
+                compress: mtc.lzmaEncode,
+                decompress: mtc.lzmaDecode
+            });
+
+            return mtc;
+        });
+    }
+    return initPromise;
+}
+
